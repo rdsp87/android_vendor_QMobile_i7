44c41740b942cc0047e55045c3e2c2bec95bca6f
Hassams <hassam363@gmail.com>
2014-12-24 20:53:43 +0500

    Update i7-vendor.mk

--- i7-vendor.mk
+++ i7-vendor.mk
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/google/i7/i7-vendor-blobs.mk)
+$(call inherit-product, vendor/QMobile/i7/i7-vendor-blobs.mk)
